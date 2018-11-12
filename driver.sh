3575016c0d623d2676752c1e90f51f80f0a73d6e
Joel Stanley <joel@jms.id.au>
2018-11-13 01:30:33 +1030

    driver: build dtbs target
    By specifying the zImage (or similar) target we miss out on other build
    artifacts. In the case of ARM boards the device trees are not built.
    
    Build it separately (but only if we're running on non-amd64, as it does
    not have a dtbs target).
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- driver.sh
+++ driver.sh
@@ -153,6 +153,7 @@ build_linux() {
   ./scripts/config -e DEBUG_SECTION_MISMATCH
   mako_reactor olddefconfig &>/dev/null
   mako_reactor ${image_name}
+  [[ $ARCH != "x86_64" ]] && mako_reactor dtbs
 
   cd "${OLDPWD}"
 }
